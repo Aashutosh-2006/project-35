25af9e38423edb41ac181a02e4373e432ce2a213
Aashutosh-2006 <72963279+Aashutosh-2006@users.noreply.github.com>
2020-12-02 13:27:12 +0545

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+//Create variables here
+var  dog, happyDog, database, foodS, foodStock;
+var d1,d2;
+
+
+function preload()
+{
+  //load images here
+  d1=loadImage("images/dogImg.png");
+  d2=loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  
+  database=firebase.database();
+   console.log(database);
+  
+  createCanvas(500, 500);
+  
+  dog=createSprite(250,270,50,50);
+  dog.addImage(d1);
+  dog.scale=0.25;
+
+  foodStock=database.ref('food');
+  foodStock.on("value",readStock);
+  
+}
+
+
+function draw() {  
+
+  background(46,139,87);
+
+if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(d2);
+}
+
+  drawSprites();
+  //add styles here
+text("Notes:Press UP_ARROW Key To Feed Drago Milk!");
+fill("blue");
+stroke("Green");
+}
+
+function writeStock(x){
+
+  if(x<=0){
+x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    food:x
+  })
+}
+
+function readStock(data){
+  foodS=data.val();
+}
